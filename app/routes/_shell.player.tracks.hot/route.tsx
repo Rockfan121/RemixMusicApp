e152f55b1655c4c642c5c5cb159b2a3c115b4a9c
Rockfan121 <k.mura121@gmail.com>
2025-11-25 08:09:51 +0100

    Add playlist with hot tracks

--- app/routes/_shell.player.tracks.hot/route.tsx
+++ app/routes/_shell.player.tracks.hot/route.tsx
@@ -1,3 +1,65 @@
+import type { MetaFunction } from "react-router";
+import { useLoaderData } from "react-router";
+import TableReplacement from "@/components/table/table-replacement";
+import TracksHeader from "@/components/table/tracks-header";
+import TracksTable from "@/components/table/tracks-table";
+import { MAX_FETCHED_ITEMS, title } from "@/config.shared";
+import { timeout300 } from "@/helpers/timeouts";
+import { hotPlaylist } from "@/services/openwhyd";
+import type { ApiPlaylist } from "@/types/openwhyd-types";
+
+const PAGE_TITLE = "Hot tracks";
+
+/**
+ * Fetch tracks from one of Openwhyd users playlists
+ */
+export const loader = async () => {
+	await new Promise(timeout300);
+	const hot_res = await fetch(hotPlaylist());
+
+	if (hot_res.status === 200) {
+		return {
+			HOT_TRACKS: await hot_res.json(),
+		};
+	}
+	return {
+		HOT_TRACKS: {},
+	};
+};
+
+export const meta: MetaFunction = () => {
+	return [{ title: title(PAGE_TITLE) }];
+};
+
 export default function HotTracks() {
-	return <h2>Hot tracks</h2>;
+	const { HOT_TRACKS } = useLoaderData<typeof loader>();
+
+	const apiplaylistInfo: ApiPlaylist = {
+		id: "hot",
+		name: PAGE_TITLE,
+		uId: "",
+		uNm: "",
+		plId: "",
+		nbTracks: MAX_FETCHED_ITEMS,
+	};
+
+	if (Object.keys(HOT_TRACKS.tracks).length === 0) {
+		return (
+			// No tracks found - the playlist is empty
+			<>
+				<TracksHeader apiplaylistInfo={apiplaylistInfo} />
+				<TableReplacement doesExist={true} />
+			</>
+		);
+	}
+
+	return (
+		// Playlist and tracks found - display them
+		<>
+			<TracksHeader apiplaylistInfo={apiplaylistInfo} />
+			<TracksTable apiplaylistInfo={apiplaylistInfo}>
+				{HOT_TRACKS.tracks}
+			</TracksTable>
+		</>
+	);
 }
