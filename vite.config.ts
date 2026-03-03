6085588d89890d064ee2c294bacd406babe4f3a3
Rockfan121 <k.mura121@gmail.com>
2026-03-03 11:04:39 +0100

    Update vite-env-only and vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import envOnly from "vite-env-only";
+import { envOnlyMacros } from "vite-env-only"
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ isSsrBuild }) => ({
@@ -15,7 +15,7 @@ export default defineConfig(({ isSsrBuild }) => ({
 	},
 	plugins: [
 		tailwindcss(),
-		envOnly(),
+		envOnlyMacros(),
 		tsconfigPaths(),
 		reactRouter(),
 		{
