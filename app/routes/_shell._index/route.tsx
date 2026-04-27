27ff38787c8b9f2539cab5c4005675444c87e5e5
Rockfan121 <k.mura121@gmail.com>
2026-04-27 04:29:22 +0200

    Refactor tailwind config Generated with Github Copilot

--- app/routes/_shell._index/route.tsx
+++ app/routes/_shell._index/route.tsx
@@ -3,7 +3,7 @@ export default function Index() {
 		<main className="flex h-screen items-center justify-center">
 			<div className="flex flex-col items-center gap-8 text-xl">
 				<h1>Welcome to</h1>
-				<h1 className="shadow-3xl shadow-primary bg-primary text-3xl font-bold">
+				<h1 className="text-shadow-3xl text-shadow-primary text-3xl font-bold">
 					RemixMusicApp!
 				</h1>
 			</div>
